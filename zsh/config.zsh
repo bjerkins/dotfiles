5d27d1352ad71eb44cbb404d250fc5c119227b32
Bjarki <bjerkins8@gmail.com>
2016-02-14 12:01:00 +0100

    show/hide files alias

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -3,6 +3,8 @@ ZSH_THEME=robbyrussell
 
 # aliases
 alias reload!='. ~/.zshrc'
+alias showfiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder'
+alias hidefiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder'
 
 # plugins
 plugins=(
